979bce0340463e38491f70f6f40c5556302484ec
getraid <32432718+getraid@users.noreply.github.com>
2024-08-17 00:32:27 +0200

    Added OutOfBoundsController

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+   // Placeholder for now. Fill in as needed
+   // Good for having mutal events, or stuff
+}
