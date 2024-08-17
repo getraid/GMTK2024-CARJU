9dab56d564c15e656494999687c89dacbc82ed9f
getraid <32432718+getraid@users.noreply.github.com>
2024-08-18 01:46:54 +0200

    Added delegate

--- Assets/Scripts/Interfaces/IDestroyable.cs
+++ Assets/Scripts/Interfaces/IDestroyable.cs
@@ -1,11 +1,13 @@
 using System;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace Interfaces
 {
     public interface IDestroyable
     {
-        public event EventHandler DestructionEvent;
+        public delegate void DestroyableDelegate(GameObject sender, EventArgs e);
+        public event DestroyableDelegate DestructionEvent;
 
      
     }
