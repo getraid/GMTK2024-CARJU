a2e13c72bac95befe36b7969a7f8209073e59809
getraid <32432718+getraid@users.noreply.github.com>
2024-08-18 14:57:00 +0200

    Civilian Spawner

--- Assets/Scripts/CilvianSpawner.cs
+++ Assets/Scripts/CilvianSpawner.cs
@@ -7,6 +7,7 @@ using Interfaces;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.Splines;
 
 [Serializable]
@@ -15,7 +16,22 @@ public struct Spawnable {
     public int count;
     public float3 spawn_offset;
     public Vector3 scale;
-    
+    public float moveSpeed;
+}
+
+
+[Serializable]
+public class GoWithIndex {
+    public GameObject currentGameObject;
+    public int knot;
+    public Spawnable typeInfo;
+
+    public GoWithIndex(GameObject gameobj, int knotI, Spawnable typeInfoS)
+    {
+        currentGameObject = gameobj;
+        knot = knotI;
+        typeInfo = typeInfoS;
+    }
 }
 
 
@@ -28,7 +44,11 @@ public class CilvianSpawner : MonoBehaviour
 
     public List<Spawnable> Spawnables; //type,count
     public bool ShouldMoveAlongPath = true;
-    private List<GameObject> currentGameObjects;
+    
+    private List<GoWithIndex> currentGameObjects;
+
+    public bool toggleUpdate = false;
+    float timer = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -40,7 +60,7 @@ public class CilvianSpawner : MonoBehaviour
         Path = SplineContainer[0];
         
         // Holds all current objs on the track
-        currentGameObjects = new List<GameObject>();
+        currentGameObjects = new List<GoWithIndex>();
 
         SpawnAllEntries();
         
@@ -56,9 +76,9 @@ public class CilvianSpawner : MonoBehaviour
 
     private IDestroyable FetchDestroyablesFromCurrentObjects()
     {
-        foreach (var vars in currentGameObjects)
+        foreach (var currentGo in currentGameObjects)
         {
-            return FetchDestroyablesFromGameObject(vars);
+            return FetchDestroyablesFromGameObject(currentGo.currentGameObject);
         }
         return null;
     }
@@ -81,10 +101,10 @@ public class CilvianSpawner : MonoBehaviour
 
     private void FindDestroyablesAndUnsubscribeSingle(GameObject obj)
     {
-        var elm = currentGameObjects.FirstOrDefault(el => el.gameObject == obj);
-        if (elm == null) return;
+        var elm = currentGameObjects.FirstOrDefault(el => el.currentGameObject.gameObject == obj);
+        if (elm.currentGameObject == null) return;
 
-        var destroyable = FetchDestroyablesFromGameObject(elm);
+        var destroyable = FetchDestroyablesFromGameObject(elm.currentGameObject);
         destroyable.DestructionEvent -= InterfOnDestructionEvent;   
         
     }
@@ -97,10 +117,10 @@ public class CilvianSpawner : MonoBehaviour
     
     private void InterfOnDestructionEvent(object sender, EventArgs e)
     {
-        GameObject gameObj = currentGameObjects.FirstOrDefault(el => el == (GameObject)sender);
+        GameObject gameObj = (currentGameObjects.FirstOrDefault(el => el.currentGameObject == (GameObject)sender)).currentGameObject;
         var tmp = Spawnables.FirstOrDefault(el => gameObj);
         
-        SpawnOnRandomPoint(gameObj, tmp.spawn_offset, tmp.scale);
+        SpawnOnRandomPoint(gameObj, tmp);
         
         FindDestroyablesAndUnsubscribeSingle(gameObj);
     }
@@ -111,59 +131,121 @@ public class CilvianSpawner : MonoBehaviour
         {
             var objType = item.type;
             var amount = item.count;
-            var offset = item.spawn_offset;
-            var scale = item.scale;
+
             for (int i = 0; i < amount; i++)
             {
-                SpawnOnRandomPoint(objType,offset,scale);
+                SpawnOnRandomPoint(objType, item);
             }
         }
     }
 
-    public void SpawnOnRandomPoint(GameObject typeOfGoToSpawn, float3 ObjectSpawnOffset, Vector3 scale)
+    public int GetValidKnotIndex(int index) => index % Path.Count;
+    
+    
+    
+    
+    public void SpawnOnRandomPoint(GameObject typeOfGoToSpawn, Spawnable spawnable)
     {
         var nextKnotRng = new System.Random();
         var nextKnot = nextKnotRng.Next();
-        var maxKnots = Path.Count;
-        
-        var knot = Path.Next(nextKnot%maxKnots);
+        var knotIndex = GetValidKnotIndex(nextKnot);
+        var knot = Path.Next(knotIndex);
         
         //next pos;
        // var nextPos= knot.Position;
        
        GameObject gameobj = Instantiate(typeOfGoToSpawn,CivilianContainer);
        float3 localTransformOffset = new float3(SplineContainer.transform.position.x,SplineContainer.transform.position.y,SplineContainer.transform.position.z);
-       gameobj.transform.localPosition= knot.Position + localTransformOffset + ObjectSpawnOffset;
+       gameobj.transform.localPosition= knot.Position + localTransformOffset + spawnable.spawn_offset;
        // gameobj.transform.position.Scale(scale);
 
        gameobj.SetActive(true);
        
        IEnumerator tmp()
        {
-           if (scale != Vector3.zero)
+           if (spawnable.scale != Vector3.zero)
            {
                yield return new WaitForEndOfFrame();
-               gameobj.transform.localScale = scale;
+               gameobj.transform.localScale = spawnable.scale;
            }
        }
 
        StartCoroutine(tmp());
       
-       
-
-       currentGameObjects.Add(gameobj);
+       currentGameObjects.Add(new GoWithIndex(gameobj, knotIndex,spawnable));
        
     }
+
+    private Vector3 ConvertKnotPosToVec3(BezierKnot knot, float3 ObjectSpawnOffset )
+    {
+        var knotPos = knot.Position;
+        float3 localTransformOffset = new float3(SplineContainer.transform.position.x,SplineContainer.transform.position.y,SplineContainer.transform.position.z);
+        return new Vector3(knotPos.x+ObjectSpawnOffset.x+localTransformOffset.x,knot.Position.y+ObjectSpawnOffset.y+localTransformOffset.y,knot.Position.z+ObjectSpawnOffset.z+localTransformOffset.z);
+    }
+
+    private Vector3 ConvertTransformPosToVec3(Transform knot, float3 ObjectSpawnOffset )
+    {
+        float3 localTransformOffset = new float3(SplineContainer.transform.position.x,SplineContainer.transform.position.y,SplineContainer.transform.position.z);
+        return new Vector3(knot.localPosition.x+ObjectSpawnOffset.x+localTransformOffset.x,knot.localPosition.y+ObjectSpawnOffset.y+localTransformOffset.y,knot.localPosition.z+ObjectSpawnOffset.z+localTransformOffset.z);
+    }
+
+
     
+
+    public void MoveGameObjectToNextPosition(GoWithIndex go)
+    {
+        var current = Path.Next(go.knot);
+        var next =  Path.Next(Path.NextIndex(go.knot)); 
     
+        MoveToKnot(go.currentGameObject, go, current, next);
+
+    }
+
+    private void MoveToKnot(GameObject gameObject,GoWithIndex go, BezierKnot from, BezierKnot to)
+    {
+        Vector3 fromPos = ConvertKnotPosToVec3(from, go.typeInfo.spawn_offset);
+        Vector3 toPos = ConvertKnotPosToVec3(to, go.typeInfo.spawn_offset);
+        
+        IEnumerator LerpWithCompletionAction(Action onCompletion){
+            while(Vector3.Distance(fromPos, gameObject.transform.localPosition)<0.001f)       
+            {
+              
+                yield return null;
+            }
+
+            if (onCompletion != null)
+            {
+                onCompletion();
+            }
+        }
+        
+        StartCoroutine(LerpWithCompletionAction(() => { UpdateToNext(go);}));
+        
+        // gameObject.transform.localPosition = (toPos - ConvertTransformPosToVec3(gameObject.transform, go.typeInfo.spawn_offset)) / go.typeInfo.moveSpeed*Time.deltaTime;
+
+        gameObject.transform.localPosition = Vector3.Lerp(fromPos, toPos, timer / 5);
+
+    }
+
+    private void UpdateToNext(GoWithIndex go)
+    {
+        Debug.Log(go.knot);
+        if(toggleUpdate == true)
+            go.knot = Path.NextIndex(go.knot); // update next knot
+        Debug.Log(go.knot);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        //TODO: Obj pooler / respawner
-
-        // Detect if obj got destroyed. First filters 
+        if (!ShouldMoveAlongPath) return;
         
+        foreach (var item in currentGameObjects)
+        {
+            MoveGameObjectToNextPosition(item);
+        }
+
+        timer += Time.deltaTime;
 
     }
 }
